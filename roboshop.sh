b86151718494470076be0549afdc768eb4242ada
sivaganeshmusulla <siva.musulla@gmail.com>
2026-08-18 14:26:58 +0530

    actions 8.7

--- roboshop.sh
+++ roboshop.sh
@@ -8,8 +8,8 @@ AMI_ID="ami-0220d79f3f480ecf5"
 for instance in $@
 
 do
-    IINSTANC_ID=$( aws ec2 run -instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SG_ID /
-    --tag specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text )
+    IINSTANC_ID=$( aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SG_ID \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text )
     if [ $instance == "frontend" ]; then
         IP=$(
             aws ec2 describe-instances --instance-ids $IINSTANC_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text
